a34c03deb383f3f731e266dc3afbb7bc29cef540
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-10 12:57:26 -0500

    first commit

---
+++ src/main/java/com/steelezide/jvServe/JvServer.java
@@ -0,0 +1,11 @@
+package com.steelezide.jvServe;
+
+/**
+ * JvServer, A lightweight Web Server class designed to allow for
+ * easy implementation of a web-server. Handles web-sockets and 
+ * multi-threading for you! Default port is 80. 
+ */
+public class JvServer {
+
+    
+}
